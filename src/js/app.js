a041f83485f94caebef99278102c219bcd22a721
Luciano-C <luciano.cabrales@gmail.com>
2022-03-08 19:53:49 +0000

    Initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import "../style/index.css";
 
-/**
+/* *
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
  * 
@@ -22,17 +22,18 @@ import "../style/index.css";
         city: null
     }
  */
+
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
+          <h1>${variables.name === null ? "" : variables.name}</h1>
           <h1>Lucy Boilett</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
